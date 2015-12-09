6bb5dcd8bb2df3b9471a8c9a0e9eac078df7ccc5
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-12-09 13:13:12 -0800

    streamlined artist seeding

--- musicdb/db/seeds.rb
+++ musicdb/db/seeds.rb
@@ -6,22 +6,42 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'rspotify'
-test = [['Arctic Monkeys', 'group'], ['Ed Sheeran', 'indiv']]
-arctic_monkeys = RSpotify::Artist.search(test[0][0]).first
-ed_sheeran = RSpotify::Artist.search(test[1][0]).first
+individuals = [{name: 'Ed Sheeran', birth_date: '1991-02-17', primary_instrument: 'Guitar', vocals: 'Lead'},
+              {name: 'James Hetfield', birth_date: '1963-08-03', primary_instrument: 'Rhythm Guitar', vocals: 'Lead'},
+              {name: 'Kirk Hammet', birth_date: '1962-11-18', primary_instrument: 'Lead Guitar', vocals: 'Background'},
+              {name: 'Lars Ulrich', birth_date: '1963-12-26', primary_instrument: 'Drums'},
+              {name: 'Cliff Burton', birth_date: '1962-02-10', primary_instrument: 'Bass Guitar', vocals: 'Background'},
+              {name: 'Jason Newsted', birth_date: '1963-03-04', primary_instrument: 'Bass Guitar', vocals: 'Background'},
+              {name: 'Robert Trujillo', birth_date: '1964-08-23', primary_instrument: 'Bass Guitar', vocals: 'Background'},
+              {name: 'Marhsall Mathews III', birth_date: '1972-10-17', primary_instrument: 'Keyboard', secondary_instrument: 'Sampler', vocals: 'Lead'}
+              ]
+groups = [{name: 'Ed Sheeran', formation_date: '2005-01-01', disband_date: nil},
+          {name: 'Metallica', formation_date: '1981-01-01', disband_date: nil},
+          {name: 'Eminem', formation_date: '1992-01-01', disband_date: nil}
+         ]
 
-test.each do |t|
-  if t[1] == 'group'
-    Group.create!({:name => t[0], :formation_date => nil, :disband_date => nil})
-  else
-    Individual.create!({:name => t[0]})
-  end
-end
+individuals.each { |person| Individual.create!(person) }
 
-arctic_monkeys.albums.each do |a|
-  album = Release.create!({:name => a.name, :date => a.release_date})
-  a.tracks.each do |t|
-    song = Song.create!({:release_id => nil, :name => t.name, :genre => a.genres, :length => t.duration_ms, :bpm => nil, :medium => nil})
-    Track.create!({:track_number => t.track_number, :song_id => song.id})
+members = {'Ed Sheeran'=> [{individual_id: Individual.where(name: 'Ed Sheeran').first.id, start_date: '2008-01-01', end_date: nil}],
+           'Metallica'=> [{individual_id: Individual.where(name: 'James Hetfield').first.id, start_date: '1981-01-01', end_date: nil},
+                         {individual_id: Individual.where(name: 'Kirk Hammet').first.id, start_date: '1983-01-01', end_date: nil},
+                         {individual_id: Individual.where(name: 'Lars Ulrich').first.id, start_date: '1981-01-01', end_date: nil},
+                         {individual_id: Individual.where(name: 'Cliff Burton').first.id, start_date: '1982-01-01', end_date: '1986-09-27'},
+                         {individual_id: Individual.where(name: 'Jason Newsted').first.id, start_date: '1987-01-01', end_date: '2001-01-01'},
+                         {individual_id: Individual.where(name: 'Robert Trujillo').first.id, start_date: '2003-01-01', end_date: nil}],
+           'Eminem'=> [{individual_id: Individual.where(name: 'Marhsall Mathews III').first.id, start_date: '1992-01-01', end_date: nil}]
+
+         }
+
+groups.each do |artist|
+  group = Group.create!(artist)
+  members[artist[:name]].each { |member| group.members.create!(member) }
+  search = RSpotify::Artist.search(artist[:name]).first
+  search.albums.each do |a|
+    release = Release.create!({:name => a.name, :date => a.release_date})
+    a.tracks.each do |t|
+      song = Song.create!({:name => t.name, :genre => a.genres, :length => t.duration_ms, :bpm => nil, :medium => nil})
+      release.tracks.create!({:track_number => t.track_number, :song_id => song.id})
+    end
   end
 end
