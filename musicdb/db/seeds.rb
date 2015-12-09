b9ccad132fa1c56368da583aa8ddaa4ed28aa9d2
susannasouv <ssouv@berkeley.edu>
2015-12-09 03:00:20 -0800

    Initial, untested seeding

--- musicdb/db/seeds.rb
+++ musicdb/db/seeds.rb
@@ -6,7 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'rspotify'
-test = RSpotify::Artist.search('Arctic Monkeys')
-arctic_monkeys = test.first # search should return a list?
+test = [['Arctic Monkeys', 'group'], ['Ed Sheeran', 'indiv']]
+arctic_monkeys = RSpotify::Artist.search(test[0][0]).first
+ed_sheeran = RSpotify::Artist.search(test[1][0]).first
 
+test.each do |t|
+  if t[1] == 'group'
+    Group.create!({:name => t[0], :formation_date => null, :disband_date => null})
+  else
+    Individuals.create!({:name => t[0], :formation_date => null, :disband_date => null})
+  end
+end
+
+arctic_monkeys.albums.each do |a|
+  a.tracks.each do |t|
+    Song.create!({:release_id => null, :name => t.name, :genre => a.genres, :length => duration_ms, :bpm => null, :medium => null})
+    Track.create!(:track_number => t.track_number, :song_id => null})
+  end
+end
 
