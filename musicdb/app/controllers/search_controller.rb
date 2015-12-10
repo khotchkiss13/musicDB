4b7732b9bf6329e9cb9fa5d0daf3fe83dff194fe
Annie Lo <annielo@berkeley.edu>
2015-12-10 13:45:33 -0800

    adv search

--- musicdb/app/controllers/search_controller.rb
+++ musicdb/app/controllers/search_controller.rb
@@ -32,61 +32,6 @@ class SearchController < ApplicationController
       @groups = Group.ransack(name_cont: q1).result
     end
 
-    q2 = params[:q2]
-    if !q2.nil?
-      num = params['num_comp2']
-      field = params['group_field2']
-      cond = params['cond2']
-      if field.is_a? Integer
-        if !num.eql?""
-          if num.eql?"<"
-          end
-          if num.eql?">"
-          end
-        end
-      end
-
-      if field.eql?"name"
-        @groups = Group.ransack(name_cont: q2).result
-      end
-    end
-
-    q3 = params[:q3]
-    if !q3.nil?
-      num = params['num_comp3']
-      field = params['group_field3']
-      cond = params['cond3']
-      if field.is_a? Integer
-        if !num.eql?""
-          if num.eql?"<"
-          end
-          if num.eql?">"
-          end
-        end
-      end
-
-      @groups = Group.ransack(name_cont: q3).result
-    end
-
-    q4 = params[:q4]
-    if !q4.nil?
-      num = params['num_comp4']
-      field = params['ind_field1']
-      cond = params['cond4']
-      if field.is_a? Integer
-        if !num.eql?""
-          if num.eql?"<"
-          end
-          if num.eql?">"
-          end
-        end
-      end
-
-      if field.eql?"name"
-        @groups = Individual.ransack(name_cont: q4).result
-      end
-    end
-
     q10 = params[:q10]
     if !q10.nil?
       num = params['num_comp10']
@@ -101,9 +46,9 @@ class SearchController < ApplicationController
         end
       end
 
-      @groups = Song.ransack(name_cont: q10).result
+      @songs = Song.ransack(name_cont: q10).result
     end
 
-    render :results
+    render :results.filter()
   end
 end
