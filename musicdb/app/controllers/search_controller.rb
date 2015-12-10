02cb641d6c5b998e241516b85f6106c05b879e05
Annie Lo <annielo@berkeley.edu>
2015-12-10 10:27:57 -0800

    SEARCH

--- musicdb/app/controllers/search_controller.rb
+++ musicdb/app/controllers/search_controller.rb
@@ -1,21 +1,15 @@
 class SearchController < ApplicationController
   def index
-    @q = Individual.ransack(params[:q])
   end
   def advanced
   end
   def results
-    @q1 = Individual.ransack(params[:q])
-    @individuals = @q1.result(distinct: true)
-    @q2 = Group.ransack(params[:q])
-    @groups = @q2.result(distinct: true)
-    @q3 = RecordLabel.ransack(params[:q])
-    @labels = @q3.result(distinct: true)
-    @q4 = Release.ransack(params[:q])
-    @releases = @q4.result(distinct: true)
-    @q5 = Show.ransack(params[:q])
-    @shows = @q5.result(distinct: true)
-    @q6 = Song.ransack(params[:q])
-    @songs = @q4.result(distinct: true)
+    q = params[:q]
+    @individuals = Individual.ransack(name_cont: q).result
+    @groups = Group.ransack(name_cont: q).result
+    @labels = RecordLabel.ransack(name_cont: q).result
+    @releases = Release.ransack(name_cont: q).result
+    @shows = Show.ransack(name_cont: q).result
+    @songs = Song.ransack(name_cont: q).result
   end
 end
