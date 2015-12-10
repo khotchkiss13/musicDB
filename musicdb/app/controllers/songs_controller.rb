cc4700a2e7da2540693c348bd3106df3094e1622
alo0822 <fangyulo@airbears2-10-142-183-152.airbears2.1918.berkeley.edu>
2015-12-10 09:47:07 -0800

    writers and producers in song form

--- musicdb/app/controllers/songs_controller.rb
+++ musicdb/app/controllers/songs_controller.rb
@@ -10,6 +10,23 @@ class SongsController < ApplicationController
 			return
 		end
 
+		writers = param[:writers]
+		writers.each do |writer|
+			individual = Individual.where(name: writer).first
+			if individual.nil?
+				redirect_to input_data_path
+			end
+			@song.writers.create(individual_id: individual.id)
+		end
+		producers = param[:producers]
+		producers.each do |producer|
+			individual = Individual.where(name: producer).first
+			if individual.nil?
+				redirect_to input_data_path
+			end
+			@song.producers.create(individual_id: individual.id)
+		end
+
 		if !@song.save
 			redirect_to input_data_path
 			return
